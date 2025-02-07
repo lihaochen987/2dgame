42f81258beec2a45ee4635c24a6f5635011b7c5d
HaoChen <lihaochen987@gmail.com>
2025-02-08 12:20:09 +1300

    yeah idk

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 
 int main() {
     // Create the main window
-    sf::RenderWindow window(sf::VideoMode({800, 600}), "SFML window");
+    sf::RenderWindow window(sf::VideoMode({800, 600}), "Epic Game");
 
     // Load a sprite to display
     sf::Texture texture;
@@ -12,11 +12,16 @@ int main() {
         // Handle loading error
         return -1;
     }
-    const sf::Sprite sprite(texture);
+    sf::Sprite sprite(texture);
+    sprite.setScale(sf::Vector2f(0.25f, 0.25f));
+
+    sf::FloatRect spriteBounds = sprite.getGlobalBounds();
+
+    sprite.setPosition(sf::Vector2f(400 - spriteBounds.position.x / 2, 300 - spriteBounds.position.y / 2));
 
     // Create a graphical text to display
     const sf::Font font("assets/arial.ttf");
-    const sf::Text text(font, "Hello SFML", 50);
+    const sf::Text text(font, "I'M IN A GAME GROMIT!", 50);
 
     // Load a music to play
     sf::Music music;
@@ -26,15 +31,41 @@ int main() {
     }
 
     // Play the music
+    music.setLooping(true);
     music.play();
 
     // Start the game loop
     while (window.isOpen()) {
+        constexpr float moveSpeed = 0.5f;
         // Process events
         while (const std::optional event = window.pollEvent()) {
             // Close window: exit
             if (event->is<sf::Event::Closed>())
                 window.close();
+        }
+
+        sf::Vector2f position = sprite.getPosition();
+        spriteBounds = sprite.getGlobalBounds();
+
+        if (isKeyPressed(sf::Keyboard::Key::Left)) {
+            if (position.x > 0) {
+                sprite.move(sf::Vector2f(-moveSpeed, 0));
+            }
+        }
+        if (isKeyPressed(sf::Keyboard::Key::Right)) {
+            if (position.x + spriteBounds.position.length() < static_cast<float>(window.getSize().x)) {
+                sprite.move(sf::Vector2f(moveSpeed, 0));
+            }
+        }
+        if (isKeyPressed(sf::Keyboard::Key::Up)) {
+            if (position.y > 0) {
+                sprite.move(sf::Vector2f(0, -moveSpeed));
+            }
+        }
+        if (isKeyPressed(sf::Keyboard::Key::Down)) {
+            if (position.y + spriteBounds.position.length() < static_cast<float>(window.getSize().y)) {
+                sprite.move(sf::Vector2f(0, moveSpeed));
+            }
         }
 
         // Clear screen
